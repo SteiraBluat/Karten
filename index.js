ee416f67c0ff70fb1c048d670db27bc35acaf87d
mondab17 <mondab17@htl-kaindorf.at>
2022-04-21 23:28:28 +0200

    Added function to display maps

---
+++ index.js
@@ -0,0 +1,167 @@
+const mapsJson = "/data/maps.json";
+const builderJson = "/data/builder.json";
+const imgDir = "/images/maps";
+
+var allMaps;
+var allBuilders;
+
+function onLoad()
+{
+    loadBuilders();
+    loadMaps();
+}
+
+function loadMaps()
+{
+    fetch(mapsJson)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data);
+            allMaps = data;
+            displayMaps(data);
+        });
+}
+
+function loadBuilders()
+{
+    fetch(builderJson)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data);
+            allBuilders = data;
+        });
+}
+
+function displayMaps(maps)
+{
+    let content = document.getElementById("maps");
+    let result;
+
+    result = "<div class='row'>";
+    for(let i = 0;i < maps.length;i++)
+    {
+        result += "<div class='col-sm-3'>" + mapToString(maps[i]) + "</div>"
+
+        if((i + 1) % 4 === 0)
+        {
+            result += "</div><div class='row'>"
+        }
+    }
+    result += "</div>";
+
+    content.innerHTML = result;
+}
+
+function mapToString(map)
+{
+    let result = "" +
+        "<div class='form-control'style='margin-bottom: 20px'>" +
+        "   <table class='table table-striped'>" +
+        "       <tr><th colspan=\"2\">" + map.name + "</th></tr>" +
+        "       <tr>" +
+        "           <td>Nr.:</td>" +
+        "           <td>" + map.id + "</td>" +
+        "       </tr>" +
+                getMapPrice(map) +
+                getMapSize(map) +
+                getMapBuilders(map) +
+                getMapCredits(map) +
+        "   </table>" +
+        "</div>";
+    return result;
+}
+
+function getMapPrice(map)
+{
+    let result = "";
+
+    if(map.priceWithAc !== 0)
+    {
+        let price = map.priceWithAc.toLocaleString("en").replaceAll(",", ".") + "$";
+        result += "" +
+            "<tr>" +
+            "   <td>Preis mit AC:</td>" +
+            "   <td>" + price + "</td>" +
+            "</tr>"
+    }
+    if(map.priceWithoutAc !== 0)
+    {
+        let price = map.priceWithoutAc.toLocaleString("en").replaceAll(",", ".") + "$";
+        result += "" +
+            "<tr>" +
+            "   <td>Preis ohne AC:</td>" +
+            "   <td>" + price + "</td>" +
+            "</tr>"
+    }
+    return result;
+}
+
+function getMapSize(map)
+{
+    let amount = map.sizeX * map.sizeY;
+
+    let result = "" +
+        "<tr>" +
+        "   <td>Anzahl der Karten:</td>" +
+        "   <td>" + amount + "</td>" +
+        "</tr>" +
+        "<tr>" +
+        "   <td>Höhe:</td>" +
+        "   <td>" + map.sizeY + "</td>" +
+        "</tr>" +
+        "<tr>" +
+        "   <td>Breite:</td>" +
+        "   <td>" + map.sizeX + "</td>" +
+        "</tr>";
+    return result;
+}
+
+function getMapBuilders(map)
+{
+    console.log(allBuilders);
+    let result = "";
+    let builders = map.builderIds;
+
+    for(let i = 0;i < builders.length;i++)
+    {
+        if(i === 0)
+        {
+            result += "" +
+                "<tr>" +
+                "   <td>Erbauer:</td>" +
+                "   <td>" + getBuilderById(builders[i]) + "</td>" +
+                "</tr>";
+        }
+        else
+        {
+            result += "" +
+                "<tr>" +
+                "   <td></td>" +
+                "   <td>" + getBuilderById(builders[i]) + "</td>" +
+                "</tr>";
+        }
+    }
+    return result;
+}
+
+function getBuilderById(id)
+{
+    let builder;
+    for(let i = 0;i < allBuilders.length;i++)
+    {
+        if(allBuilders[i].id === id)
+        {
+            builder = allBuilders[i].name;
+        }
+    }
+    return builder;
+}
+
+function getMapCredits(map)
+{
+    return "" +
+        "<tr>" +
+        "   <td>Credits:</td>" +
+        "   <td><a href='" + "https://" + map.creatorLink + "' target='_blank' rel='noopener noreferrer'>" + map.creatorLink + "</a></td>" +
+        "</tr>";
+}
